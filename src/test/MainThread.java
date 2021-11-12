37974478e7d416f8acef1904cf82794af11aca9a
yfsanchez <yfsanchez@gmail.com>
2021-11-12 18:19:21 -0300

    copy of code

--- src/test/MainThread.java
+++ src/test/MainThread.java
@@ -4,20 +4,19 @@ package test;
  *
  * @author Richard
  */
-public class Main {
+public class MainThread {
 
 	public static void main(String[] args) {
 
 		Cliente cliente1 = new Cliente("Cliente 1", new int[] { 2, 2, 1, 5, 2, 3 });
 		Cliente cliente2 = new Cliente("Cliente 2", new int[] { 1, 3, 5, 1, 1 });
 
-		Cajera cajera1 = new Cajera("Cajera 1");
-		Cajera cajera2 = new Cajera("Cajera 2");
-
 		// Tiempo inicial de referencia
 		long initialTime = System.currentTimeMillis();
+		CajeraThread cajera1 = new CajeraThread("Cajera 1", cliente1, initialTime);
+		CajeraThread cajera2 = new CajeraThread("Cajera 2", cliente2, initialTime);
 
-		cajera1.procesarCompra(cliente1, initialTime);
-		cajera2.procesarCompra(cliente2, initialTime);
+		cajera1.start();
+		cajera2.start();
 	}
 }
